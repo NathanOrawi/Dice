31630f83301398ccd66ad2629dcbf8b666868999
NathanOrawi <nathan.orawi@my.jcu.edu.au>
2024-06-06 17:40:41 +1000

    Replace TextView with ImageView - 6 dice img

--- app/src/main/java/au/edu/jcu/my/diceroller/MainActivity.kt
+++ app/src/main/java/au/edu/jcu/my/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package au.edu.jcu.my.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -20,6 +21,8 @@ class MainActivity : AppCompatActivity() {
         // Show a toast message when the app starts
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
+        // Do a dice roll when the app starts
+        rollDice()
         // Set the padding to the system bars
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -45,8 +48,22 @@ class MainActivity : AppCompatActivity() {
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        // Find the ImageView in the layout
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
+
+
     }
 }
